7b449d7db325130048aafa66abf324467e372f51
Yawar Hussain <=>
2025-05-14 13:51:08 +0500

    Project resources

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("user_input")
+
+btn.addEventListener('click', () => {
+
+    let input_value = input.value
+    console.log(input_value)
+
+})
