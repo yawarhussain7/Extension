c81607fbb45f7ee7c4d48976ff6fd0efa3520fcb
Yawar Hussain <=>
2025-05-15 14:46:32 +0500

    Chrome Extension

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("btn");
-const input = document.getElementById("user_input")
-
-btn.addEventListener('click', () => {
-
-    let input_value = input.value
-    console.log(input_value)
-
-})
