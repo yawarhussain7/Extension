1df78a0f4e20cf3dd77151223f7890f58d08a755
Yawar Hussain <=>
2025-05-15 14:59:24 +0500

    THis help to get api key from user

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(() => {
+
+    chrome.storage.sync.get(["geminiApiKey"], (result) => {
+        if (!result.geminiApiKey) {
+            chrome.tabs.create({
+                url: "options.html"
+            })
+        }
+    })
+})
